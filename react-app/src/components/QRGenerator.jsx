6eac05fb7f4a4106bcd4a621153b47388971ef7d
luisvillegasbf23 <luisville.lv@gmail.com>
2023-01-24 09:54:24 -0300

    fix: cors

--- react-app/src/components/QRGenerator.jsx
+++ react-app/src/components/QRGenerator.jsx
@@ -2,23 +2,25 @@ import React, { useState } from 'react';
 import "./QRGenerator.css"
 
 function QRGenerator() {
-    const [qrText, setQrText] = useState('');
+    const [qrText, setQrText] = useState({name: "",
+    data: "",
+    // color: "",
+    // bg_color: "",
+    // size: 500
+   });
     const [qr, setQr] = useState('');
+    const url = "https://bzc5fdl6pdp2marmkoosjiezku0aicay.lambda-url.us-east-1.on.aws"
+    // const url = "http://0.0.0.0:8000"
+    console.log(url)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             // Envía una petición POST a tu API
-            const response = await fetch('https://bzc5fdl6pdp2marmkoosjiezku0aicay.lambda-url.us-east-1.on.aws/', {
+            // const response = await fetch('https://bzc5fdl6pdp2marmkoosjiezku0aicay.lambda-url.us-east-1.on.aws/create_qr', {
+            const response = await fetch(`${url}/create_qr`, {
                 method: 'POST',
-                body: JSON.stringify({ 
-                    name: "myqr",
-                    data: "https://twitter.com/home",
-                    config: "PNG",
-                    // color: "#000000",
-                    bg_color: "#ffffff",
-                    // size: 500
-                   }),
+                body: JSON.stringify(qrText),
                 headers: { 
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
@@ -33,16 +35,53 @@ function QRGenerator() {
         }
     }
 
+    const handleChange = e => {
+        setQrText({
+          ...qrText,
+          [e.target.name]: e.target.value,
+        })
+      }
+
+    //   const deleteLink = (e) => {
+    //     e.preventDefault()
+    //     setQr("");
+
+    // }
+
+    console.log(qrText)
+
     return (
         <div className='container'>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="form">
                 <input
                     type="text"
-                    placeholder="Ingresa el texto para generar el QR"
-                    value={qrText}
-                    onChange={e => setQrText(e.target.value)}
+                    placeholder="qr name"
+                    name='name'
+                    value={qrText.name}
+                    onChange={handleChange}
                 />
-                <button type="submit">Generar QR</button>
+                <input
+                    type="text"
+                    placeholder="url"
+                    name='data'
+                    value={qrText.data}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="default #000000"
+                    name='color'
+                    value={qrText.color}
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    placeholder="default transparent"
+                    name="bg_color"
+                    value={qrText.bg_color}
+                    onChange={handleChange}
+                />
+                <button type="submit">generate qr</button>
             </form>
             {qr && <a href={qr.link} download={qr.name}>Descargar QR</a>}
         </div>
